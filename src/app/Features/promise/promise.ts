dec1e7d2c429b8848e1e7bb1ca8be4add9ad3057
Vaibhav2596 <chavanv214@gmail.com>
2026-02-07 16:35:47 +0530

    JavaScript Promise Explained

---
+++ src/app/Features/promise/promise.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promise',
+  imports: [],
+  templateUrl: './promise.html',
+  styleUrl: './promise.scss',
+})
+export class PromiseComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PromiseComponent } from './Features/promise/promise';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'promise', pathMatch: 'full' },
+  { path: 'promise', component: PromiseComponent },
+];
