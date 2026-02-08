230f7fb8eea9e08c1053105f9857c7d476799190
Vaibhav2596 <chavanv214@gmail.com>
2026-02-08 11:51:25 +0530

    Async Await in JavaScript

--- src/app/Features/async-await/async-await.ts
+++ src/app/Features/async-await/async-await.ts
@@ -1,19 +1,73 @@
+import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-async-await',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './async-await.html',
   styleUrl: './async-await.scss',
 })
 export class AsyncAwait implements OnInit {
+  resultByAsyncAwait: any;
+  resultByPromise: any;
+  fetchPostsResult:any;
+
+  dell = {
+    brand: 'Dell',
+    hardDisk: '2 TB',
+    color: 'Black',
+  };
 
   ngOnInit(): void {
-    console.log(this.getData().then(data => console.log(data)));
+    this.getData();
   }
 
-  async getData(){
-    return 'Data Received';
+  dataPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Data Received');
+    }, 3000);
+  });
+
+  buyLaptop = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(this.dell);
+    }, 3000);
+  });
+
+  fetchPosts = fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json())
+
+  // async keyword with function always return promise
+  async getData() {
+    let response = await this.dataPromise;
+    console.log(response);
   }
 
+  // Example 01 : With Promise
+  fetchByPromise() {
+    this.buyLaptop.then((res) => {
+      console.log(res);
+      this.resultByPromise = res;
+    });
+  }
+
+  // Example 02 : With Async Await
+  async fetchByAsyncAwait() {
+    let response = await this.buyLaptop;
+    console.log(response);
+    this.resultByAsyncAwait = response;
+  }
+
+  // Promise
+  // fetchByAPI(){
+  //   this.fetchPosts.then(data => {
+  //     console.log(data);
+  //     this.fetchPostsResult = data;
+  //   })
+  // }
+
+  // Async Await
+  async fetchByAPI(){
+    let result = await this.fetchPosts;
+    this.fetchPostsResult = result;
+  }
 }
